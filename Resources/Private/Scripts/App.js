316391f5eeb30963542f68fda5a9ee2106625706
ryzy <marcin@ryzycki.com>
2014-11-06 16:17:51 +0000

    Initial setup, build tools (Gulp), base templates

---
+++ Resources/Private/Scripts/App.js
@@ -0,0 +1,4 @@
+// Run Zurb Foundation
+
+// For now put it directly in HTML template, to save 1 extra request.
+$(document).foundation();
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+/* jshint strict: false */
+
+var args = require('yargs').argv;
+var del = require('del');
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')({ camelize:true });
+var config = require('./gulpconfig.json');
+
+var isProduction = /prod/i.test(args.env);
+
+
+gulp.task('styles', function() {
+	gulp.src(config.source.styles)
+		.pipe($.plumber())
+		.pipe($.sass({
+			includePaths: config.source.includePaths,
+			imagePath: config.dest.images,
+			sourceComments: 'map',
+			sourceMap: isProduction === false,
+			errLogToConsole: true
+		}))
+		.pipe($.autoprefixer('last 3 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe($.if(isProduction,
+			$.csso() // only minify/compress on Production
+		))
+		.pipe(gulp.dest(config.dest.styles))
+		.pipe($.size({ showFiles:true }))
+		.pipe($.size({ gzip:true }))
+	;
+});
+
+gulp.task('scripts', function() {
+	var scripts = config.source.vendorScripts.concat(config.source.scripts);
+	return gulp.src(scripts)
+		.pipe($.if(isProduction,
+			$.uglify({ mangle: false }) // only minify/uglify on Production
+		))
+		.pipe($.concat('App.js'))
+		.pipe(gulp.dest(config.dest.scripts))
+		.pipe($.size({ showFiles:true }))
+		.pipe($.size({ gzip:true }))
+	;
+});
+
+gulp.task('scripts-hinting', function() {
+	return gulp.src(config.source.scripts)
+		.pipe($.jshint('.jshintrc'))
+		.pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('assets', function() {
+	del(config.clear);
+	
+	return gulp.src(config.source.fonts)
+		.pipe(gulp.dest(config.dest.fonts))
+	;
+});
+
+gulp.task('build', [
+	'assets',
+	'styles',
+	'scripts-hinting',
+	'scripts'
+]);
+
+// Watch
+gulp.task('watch', function () {
+	gulp.watch(config.watch.styles, ['styles']); // Watch .scss files
+	gulp.watch(config.watch.scripts, ['scripts-hinting', 'scripts']); // Watch .js files
+});
+
+// Default Task
+gulp.task('default', ['build']);
