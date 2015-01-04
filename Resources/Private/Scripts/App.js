438faff832113ef829ed86c5f65a870a766f3f5a
ryzy <marcin@ryzycki.com>
2015-01-04 00:10:06 +0000

    JS: on Neos.PageLoaded event, Foundation is re-initialised
    With this change we have always working Foundation JS components, even after page was loaded in Neos backend via AJAX.

--- Resources/Private/Scripts/App.js
+++ Resources/Private/Scripts/App.js
@@ -4,3 +4,16 @@ $(document).ready(function(){
 	// Run Zurb Foundation
 	$(document).foundation();
 });
+
+/**
+ * Invoked when page finished loading (via AJAX) in Neos back-end.
+ */
+document.addEventListener('Neos.PageLoaded', function () {
+	'use strict';
+	
+	// Re-initialise Foundation again
+	// This is needed to properly build newly added on page JS components (slider, tabs etc)
+	// @TODO: Do we need to call it with some timeout? Seems like it works OK without it.
+	$(document).foundation();
+	
+}, false);
